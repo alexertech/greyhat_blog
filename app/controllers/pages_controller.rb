09b6829d03cf09d80f865e4b01927dd8096a8586
Alex B <ab@alexertech.com>
2019-11-15 23:55:50 -0300

    Simple web, posts scaffold, slug urls

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def index
+  end
+  def about
+  end
+  def services
+  end
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :posts
+  root 'pages#index' # Pagina principl
+
+  get '/index'     => 'pages#index'
+  get '/acerca'    => 'pages#about'
+  get '/servicios' => 'pages#services'
+  get '/contacto'  => 'pages#contact'
+
+  get '/blog'      => 'posts#list'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
