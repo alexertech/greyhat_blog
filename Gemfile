bbfc8928e1fda636aa0dc7a640bd901b2c08a9e9
Alex B <ab@alexertech.com>
2019-12-14 16:47:07 -0300

    feature: markdown, blogs done

--- Gemfile
+++ Gemfile
@@ -46,4 +46,9 @@ end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+# Auth support
 gem 'devise'
+
+# Markdown
+gem 'redcarpet'
+gem 'coderay'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :list]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+
   # GET /posts
   # GET /posts.json
   def index
@@ -10,7 +11,7 @@ class PostsController < ApplicationController
 
   # Get /blog
   def list
-    @posts = Post.all
+    @posts = Post.order("id DESC").all
   end
 
  # GET /posts/1
@@ -79,6 +80,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :posts)
+      params.require(:post).permit(:title, :body)
     end
 end
