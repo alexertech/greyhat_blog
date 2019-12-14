0bb9d863c52b2a2d23c17a2fdce3b6251ebbe8bf
Alex B <ab@alexertech.com>
2019-12-14 16:47:07 -0300

    feature: markdown, blogs done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_162956) do
+ActiveRecord::Schema.define(version: 2019_12_14_170544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_162956) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "posts"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
