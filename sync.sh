7146d6f63bec9fb93a3e29d6b228a2aa45295cd0
Alex B <ab@alexertech.com>
2020-02-05 11:39:29 -0300

    fix url

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz -e "ssh -p 7822 -i /home/alex/id_rsa_new" * alex@167.71.176.223:/var/www/blog --exclude '.git' --exclude 'sync.sh' --exclude 'tmp' --exclude 'log' --exclude 'node_modules'
+rsync -avz -e "ssh -p 7822 -i /home/alex/id_rsa_new" * alex@167.71.176.223:/var/www/blog --exclude '.git' --exclude 'sync.sh' --exclude 'tmp' --exclude 'log' --exclude 'node_modules' --exclude 'public/packs' --exclude 'storage'
