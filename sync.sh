ed2c458504872a9600409d22971bce7908a71f34
Alex B <ab@alexertech.com>
2020-10-13 16:29:16 -0300

    all images rounded

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz -e "ssh -p 7822 -i /home/alex/id_rsa_new" * alex@167.71.176.223:/var/www/blog --exclude '.git' --exclude 'sync.sh' --exclude 'tmp' --exclude 'log' --exclude 'node_modules' --exclude 'public/packs' --exclude 'storage' --exclude 'public/assets'
+rsync -avz -e "ssh -p 7822 -i ~/id_rsa_new" * alex@167.71.176.223:/var/www/blog --exclude '.git' --exclude 'sync.sh' --exclude 'tmp' --exclude 'log' --exclude 'node_modules' --exclude 'public/packs' --exclude 'storage' --exclude 'public/assets'
