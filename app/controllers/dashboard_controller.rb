dd53222201ef5a66cd6b876490f3dc11f3720eee
Alex B <ab@alexertech.com>
2022-09-16 16:38:36 -0300

    compatibility updates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @statIndex =
-      Impression.where(controller_name: "pages", action_name: "index").length
-    @statAbout =
-      Impression.where(controller_name: "pages", action_name: "about").length
-    @statServ =
-      Impression.where(controller_name: "pages", action_name: "services").length
-    @statPosts =
-      Impression.where(controller_name: "posts", action_name: "list").length
+    @statIndex =''
+    @statAbout =''
+    @statServ = ''
+    @statPosts =''
   end
 
   def stats
