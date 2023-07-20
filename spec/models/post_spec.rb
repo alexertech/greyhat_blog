a63a4ef1ee379dd3c430ef01cc5eefaf7b62c197
Alex B <ab@alexertech.com>
2023-07-20 12:25:57 -0400

    adding a post spec

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+  context 'post' do
+		it 'createa post with atachment' do
+      subject = Post.new
+
+      subject.title = 'title'
+      subject.body =  'body'
+      subject.image.attach(
+        io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'image.jpg')),
+        filename: 'image.jpg',
+        content_type: 'image/jpeg'
+      )
+      subject.save!
+
+      expect(subject).to be_valid
+      expect(subject.image).to be_attached
+      expect(subject.image).to be_an_instance_of(ActiveStorage::Attached::One)
+      expect(subject.image.blob.filename).not_to be_nil
+		end
+	end
+
+end
