fe1c42643bf3b5a3778b394bcd0ba466468ab825
Alex B <ab@alexertech.com>
2024-08-24 13:37:28 -0400

    stimulus setup

---
+++ config/importmap.rb
@@ -0,0 +1,6 @@
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/umd/react.production.min.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@17.0.2/umd/react-dom.production.min.js"
+
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
