710684e8b8fe7ff722ad9eaf53082753b59b8cfc
Alex B <ab@alexertech.com>
2025-06-07 18:28:42 -0400

    dashboard setup & search fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 # Bootstrap JS Components
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+
+# Charts
+pin "chartkick", to: "https://unpkg.com/chartkick@5.0.1/dist/chartkick.esm.js"
