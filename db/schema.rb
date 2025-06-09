77c317a4b260aa2b037fd596cc0814c76ad4ac61
Alex B <ab@alexertech.com>
2025-06-09 12:05:22 -0400

    fixing seeds

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.2].define(version: 2025_06_07_175510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "vector"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
