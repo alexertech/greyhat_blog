1b1847f908657973ffe4c0b4c9a72471d04162f9
Alex B <ab@alexertech.com>
2025-07-03 13:15:18 -0400

    importmap review

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,8 +6,10 @@ import "bootstrap"
 import "trix"
 import "@rails/actiontext"
 import "@rails/activestorage"
-import "./charts"
 import "@hotwired/turbo-rails"
+
+// Import charts functionality directly
+import "./charts"
 
 // Remove Rails UJS as it conflicts with Turbo
 // Rails UJS is not needed when using Turbo Rails
