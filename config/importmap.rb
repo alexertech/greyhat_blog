ec1c7ce546826559457762a3c242b11d6a16dc3f
Alex B <ab@alexertech.com>
2025-07-03 12:56:08 -0400

    importmap update

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "application", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true  
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "charts", to: "charts.js", preload: true
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@rails/activestorage", to: "activestorage.esm.js"
