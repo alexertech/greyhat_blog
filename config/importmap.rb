12f6814ffefea46c364d077a6b3bbca98b65083c
Alex B <ab@alexertech.com>
2025-07-03 13:07:05 -0400

    importmap cleanup

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,6 @@ pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true  
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "charts", to: "charts.js", preload: true
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@rails/activestorage", to: "activestorage.esm.js"
