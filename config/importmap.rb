4e40a49de4ef0eda165006507795f425b5930d5e
Alex B <ab@alexertech.com>
2025-07-03 12:46:33 -0400

    Dashboard refactor + updates

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,10 @@ pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@rails/activestorage", to: "activestorage.esm.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
+# Chart.js
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.js"
+pin "@kurkle/color", to: "https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.2/dist/color.esm.js"
+
 # Bootstrap JS Components
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
