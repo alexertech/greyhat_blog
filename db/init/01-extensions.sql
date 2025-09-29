5b0563173f42fcd3ffca4eda87bd54abd6ea4149
Alex B <ab@alexertech.com>
2025-09-29 14:21:16 -0300

    Docker review +6

--- db/init/01-extensions.sql
+++
@@ -1,6 +0,0 @@
--- PostgreSQL extensions required by the application
-CREATE EXTENSION IF NOT EXISTS plpgsql;
-
--- Note: User roles and passwords should be managed via environment variables
--- The postgres user is created automatically by the postgres Docker image
--- Additional roles should be created via POSTGRES_USER and POSTGRES_PASSWORD env vars
